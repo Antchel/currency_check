d3cd6e1b1fb5296f39272aa5caa32d2a56fbf090
Antchel <golan349@gmail.com>
2020-04-16 18:07:51 +0500

    Conter

--- app/src/main/java/com/example/currency_check/MainActivity.kt
+++ app/src/main/java/com/example/currency_check/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.currency_check
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun mytoast (view: View)
+    {
+        val mytoast = Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT)
+        mytoast.show()
+    }
+    fun countMe (view: View)
+    {
+        val string = textView.text.toString()
+        var count: Int = string.toInt()
+        count++
+        textView.text = count.toString()
+
     }
 }
